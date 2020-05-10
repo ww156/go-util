64d9a137d345799cd637628a3ba1d1f6aa7e51e5
王威 <luobo@ecook.cn>
2020-05-10 14:20:23 +0800

    修改测试代码

--- sha1_test.go
+++ sha1_test.go
@@ -6,11 +6,13 @@ import (
 )
 
 func TestSha1(t *testing.T) {
+	str := "123"
 	//c04483ac5c934f62569c0fcb6fd75a1dd6b4fe5a
-	fmt.Println(Sha1(STR))
+	fmt.Println(Sha1(str))
 }
 
 func TestSha1File(t *testing.T) {
+	file := "./test.txt"
 	//c04483ac5c934f62569c0fcb6fd75a1dd6b4fe5a
-	fmt.Println(Sha1File(FILE))
+	fmt.Println(Sha1File(file))
 }
